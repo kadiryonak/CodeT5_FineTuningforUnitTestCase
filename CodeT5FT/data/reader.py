0daae618790e882da10cbf1168f6a9e515899e72
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2025-08-24 17:41:14 +0300

    Update reader.py

--- CodeT5FT/data/reader.py
+++ CodeT5FT/data/reader.py
@@ -2,13 +2,13 @@ import os
 import json
 from abc import ABC, abstractmethod
 
-# DataReader arayüzü
+
 class DataReader(ABC):
     @abstractmethod
     def read_data(self):
         pass
 
-# JSON dosyaları okuma
+
 class JsonDataReader(DataReader):
     def __init__(self, data_path):
         self.data_path = data_path
@@ -17,14 +17,14 @@ class JsonDataReader(DataReader):
         self.test = []
 
     def read_data(self):
-        print("Veri okunmaya başlandı...")
+        print("Dataset start to reading...")
 
         for root, dirs, files in os.walk(self.data_path):
             for filename in files:
                 if filename.endswith('.json'):
                     file_path = os.path.join(root, filename)
 
-                    # Klasör adlarına göre verileri ilgili değişkenlere ata
+
                     if 'train' in root:
                         self._load_json(file_path, 'train')
                     elif 'eval' in root:
@@ -32,14 +32,14 @@ class JsonDataReader(DataReader):
                     elif 'test' in root:
                         self._load_json(file_path, 'test')
 
-        # Boş veri kontrolü
+  
         if not self.train and not self.eval and not self.test:
-            print("Hata: JSON dosyası bulunamadı veya boş.")
+            print("Error: Json folder is not found.")
 
         return self.train, self.eval, self.test
 
     def _load_json(self, file_path, root):
-        print(f"Yükleniyor: {file_path}")
+        print(f"Loading: {file_path}")
         try:
             with open(file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
@@ -50,6 +50,6 @@ class JsonDataReader(DataReader):
                 elif 'test' in root:
                     self.test.append(data)
         except json.JSONDecodeError:
-            print(f"Hata: JSON dosyası çözümlenirken bir hata oluştu: {file_path}")
+            print(f"Error: An error occurred while parsing the JSON file: {file_path}")
         except FileNotFoundError:
-            print(f"Hata: Dosya bulunamadı: {file_path}")
+            print(f"Error: File not found: {file_path}")
